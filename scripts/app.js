273b42329a7528a896d1ae2ef17d42663af3d9aa
niklerk <niklerk@DESKTOP-0PHNLLB>
2020-06-13 20:51:12 -0500

    Changes to app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,33 @@
+
 function App() {
     return (
-      <div className="App">
+        <div className="App">
         <h1>Prueba Hoteles</h1>
-      </div>
+        <Header />
+        <Filtros />
+        <Hoteles />
+
+        </div>
     );
-  }
+}
+
+const Header = () =>{
+    return(
+        <h1>Header</h1>
+    )
+}
+
+const Filtros = () =>{
+    return(
+        <h1>Filtros</h1>
+    )
+    }
+
+const Hoteles = () =>{
+    return(
+        <h1>Hoteles</h1>
+    )
+}
   
   const rootElement = document.getElementById("app");
   ReactDOM.render(<App />, rootElement);
