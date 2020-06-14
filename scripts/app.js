2a6866a974d0b5439acc9c9adc4e35b08558602d
niklerk <niklerk@DESKTOP-0PHNLLB>
2020-06-13 20:01:35 -0500

    changes to architecture

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
-class App extends React.Component{
-render(){
-    return(
-            <h1>Prueba</h1>
-    )
-}
-}
-
-ReactDOM.render(<App />, document.getElementById('app'));
+function App() {
+    return (
+      <div className="App">
+        <h1>Prueba Hoteles</h1>
+      </div>
+    );
+  }
+  
+  const rootElement = document.getElementById("app");
+  ReactDOM.render(<App />, rootElement);
+  
