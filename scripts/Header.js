7e9b30b76ec62c27675c6ef0676ccc68035e9f5e
Elkin Cardoso <elkin.cardoso@globant.com>
2020-06-25 20:37:48 -0500

    Cahnges styles

--- scripts/Header.js
+++ scripts/Header.js
@@ -7,7 +7,7 @@ class Header extends React.Component {
         return (
             <div className="header">
                 <h3>Hoteles  </h3>
-                <span>Desde <b>{initialDate}</b> hasta <b>{endDate}</b></span>
+                <span>Desde el <b>{initialDate}</b> hasta el <b>{endDate}</b></span>
 
             </div>
         )
