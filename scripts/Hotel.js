d9ae7f1ee60d26849a867310f820c599f1755b45
Elkin Cardoso <elkin.cardoso@globant.com>
2020-06-29 14:06:31 -0500

    iteracion icono pesos

--- scripts/Hotel.js
+++ scripts/Hotel.js
@@ -1,4 +1,14 @@
 function Hotel(props){
+
+  function Icons({ icon, iconPrice }) {
+    let content = [];
+      for (let i = 0; i < iconPrice; i++) {
+        content.push(
+          <i className={"fa " + icon} key={"icon-" + i}></i>
+        );
+      }
+    return content;
+  }
         return (
             <div className="item">
               <div className="image">
@@ -9,20 +19,19 @@ function Hotel(props){
                     <p>{props.description}</p>
                     <p><i className="fa fa-map-marker"></i> {props.city}, {props.country}</p>
                     <div className="bothElements" >
-                    <p className="percentage"><i className="fa fa-bed" ></i> {props.rooms} Habitaciones</p>
-                    <p className="percentage"><i className="fa fa-usd" ></i> {props.price} </p>
+                      <p className="percentage"><i className="fa fa-bed" ></i> {props.rooms} Habitaciones</p>
+                      <p className="percentage"><Icons icon="fa-usd" iconPrice={props.price} /></p>
                     </div>
-                </div>
-                <div>
-                <button
-                        id="btn"
-                        className="btn"
-                        type="submit"
-                    >
-                    RESERVAR
-                    </button>
+                  <div>
+                  <button
+                          id="btn"
+                          className="btn"
+                          type="submit"
+                      >
+                      RESERVAR
+                      </button>
+                  </div>
                 </div>
             </div>
-          );
-        
-    }
+        );
+}
